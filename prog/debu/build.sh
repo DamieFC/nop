274be4fd28d1ad1086c63ef33529c2c9847e3554
segfaultdev <segfaultdev@protonmail.com>
2021-10-10 00:50:14 +0200

    misc: FIXED THE ENTIRE DAMN OS

--- prog/debu/build.sh
+++ prog/debu/build.sh
@@ -3,5 +3,5 @@
 clang -Os --target=i586-unknown-elf -march=i586 -fsanitize=undefined,unsigned-integer-overflow,local-bounds -ffreestanding -fno-builtin -nostdlib -I include -I ../../libnop/include $(find . -name "*.c") -c
 find . -name "*.asm" -exec nasm -O4 -felf {} \;
 
-i586-elf-ld -T linker.ld -nostdlib $(find . -name "*.o") $(find ../../libnop -name "*.o") -o debu.nex
-rm -rf *.o
+i586-elf-ld -T link_bin.ld -nostdlib $(find . -name "*.o") $(find ../../libnop -name "*.o") -o debu.nex
+i586-elf-ld -T link_elf.ld -nostdlib $(find . -name "*.o") $(find ../../libnop -name "*.o") -o debu.elf
